202b19895d9241cb2c46010818d9b3febdb6dddb
Bryan CHAVARRIA <b.chavarria@lacroix.group>
2022-11-28 16:33:12 -0600

    Add splash screen

--- App.js
+++ App.js
@@ -1,14 +1,57 @@
-import { StatusBar } from "expo-status-bar";
+import { useCallback, useEffect, useState } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [appIsLoaded, setAppIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const prepare = async () => {
+      try {
+        await Font.loadAsync({
+          black: require("./assets/fonts//Roboto-Black.ttf"),
+          blackItalic: require("./assets/fonts/Roboto-BlackItalic.ttf"),
+          bold: require("./assets/fonts/Roboto-Bold.ttf"),
+          boldItalic: require("./assets/fonts/Roboto-BoldItalic.ttf"),
+          italic: require("./assets/fonts/Roboto-Italic.ttf"),
+          light: require("./assets/fonts/Roboto-Light.ttf"),
+          lightItalic: require("./assets/fonts/Roboto-LightItalic.ttf"),
+          medium: require("./assets/fonts/Roboto-Medium.ttf"),
+          mediumItalic: require("./assets/fonts/Roboto-MediumItalic.ttf"),
+          regular: require("./assets/fonts/Roboto-Regular.ttf"),
+          thin: require("./assets/fonts/Roboto-Thin.ttf"),
+          thinItalic: require("./assets/fonts/Roboto-ThinItalic.ttf"),
+        });
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setAppIsLoaded(true);
+      }
+    };
+    prepare();
+  }, []);
+
+  const onLayout = useCallback(async () => {
+    if (appIsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [appIsLoaded]);
+
+  if (!appIsLoaded) {
+    return null;
+  }
+
   return (
-    <SafeAreaProvider style={styles.container}>
+    <SafeAreaProvider style={styles.container} onLayout={onLayout}>
       <SafeAreaView>
-        <Text>BorderChat app!</Text>
+        <Text style={styles.label}>Welcome to the BorderChat app!</Text>
         <StatusBar style="auto" />
-        <Button title="Click me" />
+        <Button title="Start chatting" />
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -20,5 +63,10 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     justifyContent: "center",
     alignItems: "center",
+  },
+  label: {
+    color: "black",
+    fontSize: 18,
+    fontFamily: "regular",
   },
 });
