199037e75cab3159631f659c9b6f24b5793ddb04
Bryan CHAVARRIA <b.chavarria@lacroix.group>
2023-02-21 15:15:07 -0700

    update sign in, sign up forms (up to lesson 51)

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Image, ScrollView, KeyboardAvoidingView, Platform } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import PageContainer from "../components/PageContainer";
@@ -7,23 +7,35 @@ import SignInForm from "../components/SignInForm";
 import SignUpForm from "../components/SignUpForm";
 import colors from "../constants/colors";
 
+import logo from "../assets/images/logo.png";
+
 const AuthScreen = (props) => {
   const [isSignUp, setIsSignUp] = useState(false);
 
   return (
     <SafeAreaView style={styles.safeArea}>
       <PageContainer style={styles.pageContainer}>
-        { 
-          isSignUp ?
-          <SignUpForm /> :
-          <SignInForm />
-        }
+        <ScrollView>
+          <KeyboardAvoidingView
+            style={styles.keyboardAvoidingView}
+            behavior={Platform.OS === "ios" ? "height" : undefined}
+            keyboardVerticalOffset={100}>
+            <View style={styles.imageContainer}>
+              <Image style={styles.image} source={logo} resizeMode="contain" />
+            </View>
 
-        <TouchableOpacity 
-          onPress={() => setIsSignUp(prevState => !prevState)}
-          style={styles.linkContainer}>
-          <Text style={styles.link}>{ `${isSignUp ? "Already a member? Sign In" : "Not a member? Sign Up"}` }</Text>
-        </TouchableOpacity>
+            {isSignUp ? <SignUpForm /> : <SignInForm />}
+
+            <TouchableOpacity
+              onPress={() => setIsSignUp((prevState) => !prevState)}
+              style={styles.linkContainer}
+            >
+              <Text style={styles.link}>{`${
+                isSignUp ? "Already a member? Sign In" : "Not a member? Sign Up"
+              }`}</Text>
+            </TouchableOpacity>
+          </KeyboardAvoidingView>
+        </ScrollView>
       </PageContainer>
     </SafeAreaView>
   );
@@ -35,14 +47,25 @@ const styles = StyleSheet.create({
   },
   pageContainer: {},
   linkContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginVertical: 15,
   },
   link: {
     color: colors.blue,
-    fontFamily: 'medium',
+    fontFamily: "medium",
     letterSpacing: 0.3,
+  },
+  imageContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: "60%",
+  },
+  keyboardAvoidingView:{
+    flex: 1,
+    justifyContent: 'center',
   },
 });
 
