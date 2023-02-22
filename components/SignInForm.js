e80ff4d99f937079f20901b2a202cef9ac1d4c51
Bryan CHAVARRIA <b.chavarria@lacroix.group>
2023-02-22 16:57:49 -0700

    update sign in/up forms (up to lesson 63)

--- components/SignInForm.js
+++ components/SignInForm.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useCallback, useReducer } from "react";
 import { StyleSheet } from "react-native";
 import { Feather } from "@expo/vector-icons";
 
 import Input from "../components/Input";
 import SubmitButton from "../components/SubmitButton";
+import { validateInput } from "../utils/actions/formActions";
+import { reducer } from "../utils/reducers/formReducer";
+
+const initialState = {
+  inputValidities: {
+    email: false,
+    password: false,
+  },
+  formIsValid: false
+}
 
 const SignInForm = (props) => {
-  const inputChangedHandler = (inputId, inputValue) => {
-    if (inputId === "firstName" || inputId === "lastName") {
-      console.log(validateString(inputId, inputValue));
-    } else if (inputId === "email") {
-      console.log(validateEmail(inputId, inputValue));
-    } else if (inputId === "password") {
-      console.log(validatePassword(inputId, inputValue));
-    }
-  };
+  const [formState, dispatchFormState] = useReducer(reducer, initialState);
+
+  const inputChangedHandler = useCallback((inputId, inputValue) => {
+    const result = validateInput(inputId, inputValue);
+    dispatchFormState({ inputId, validationResult: result })
+  }, [dispatchFormState]);
 
   return (
     <>
@@ -27,6 +34,7 @@ const SignInForm = (props) => {
         iconSize={24}
         iconPack={Feather}
         onInputChanged={inputChangedHandler}
+        errorText={formState.inputValidities["email"]}
       />
       <Input
         id="password"
@@ -37,11 +45,13 @@ const SignInForm = (props) => {
         iconSize={24}
         iconPack={Feather}
         onInputChanged={inputChangedHandler}
+        errorText={formState.inputValidities["password"]}
       />
       <SubmitButton
         title="Sign In"
         style={styles.submitButton}
         onPress={() => console.log("Button pressed")}
+        disabled={!formState.formIsValid}
       />
     </>
   );
--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -1,71 +1,83 @@
-import React from "react";
+import React, { useCallback, useReducer } from "react";
 import { StyleSheet } from "react-native";
 import { FontAwesome, Feather } from "@expo/vector-icons";
 
 import Input from "../components/Input";
 import SubmitButton from "../components/SubmitButton";
-import {
-  validateString,
-  validateEmail,
-  validatePassword,
-} from "../utils/validationConstraints";
+import { validateInput } from "../utils/actions/formActions";
+import { reducer } from "../utils/reducers/formReducer";
 
-const SignUpForm = props => {
-  const inputChangedHandler = (inputId, inputValue) => {
-    if (inputId === "firstName" || inputId === "lastName") {
-      console.log(validateString(inputId, inputValue));
-    } else if (inputId === "email") {
-      console.log(validateEmail(inputId, inputValue));
-    } else if (inputId === "password") {
-      console.log(validatePassword(inputId, inputValue));
-    }
-  }
+const initialState = {
+  inputValidities: {
+    firstName: false,
+    lastName: false,
+    email: false,
+    password: false,
+  },
+  formIsValid: false,
+};
 
-    return (
-      <>
-        <Input
-          id="firstName"
-          label="First Name"
-          icon="user-o"
-          iconSize={24}
-          iconPack={FontAwesome}
-          onInputChanged={inputChangedHandler}
-        />
-        <Input
-          id="lastName"
-          label="Last Name"
-          icon="user-o"
-          iconSize={24}
-          iconPack={FontAwesome}
-          onInputChanged={inputChangedHandler}
-        />
-        <Input
-          id="email"
-          label="Email"
-          icon="mail"
-          autoCapitalize="none"
-          keyboardType="email-address"
-          iconSize={24}
-          iconPack={Feather}
-          onInputChanged={inputChangedHandler}
-        />
-        <Input
-          id="password"
-          label="Password"
-          icon="lock"
-          autoCapitalize="none"
-          secureTextEntry
-          iconSize={24}
-          iconPack={Feather}
-          onInputChanged={inputChangedHandler}
-        />
-        <SubmitButton
-          title="Sign Up"
-          style={styles.submitButton}
-          onPress={() => console.log("Button pressed")}
-        />
-      </>
-    );
+const SignUpForm = (props) => {
+  const [formState, dispatchFormState] = useReducer(reducer, initialState);
+
+  const inputChangedHandler = useCallback(
+    (inputId, inputValue) => {
+      const result = validateInput(inputId, inputValue);
+      dispatchFormState({ inputId, validationResult: result });
+    },
+    [dispatchFormState]
+  );
+
+  return (
+    <>
+      <Input
+        id="firstName"
+        label="First Name"
+        icon="user-o"
+        iconSize={24}
+        iconPack={FontAwesome}
+        onInputChanged={inputChangedHandler}
+        errorText={formState.inputValidities["firstName"]}
+      />
+      <Input
+        id="lastName"
+        label="Last Name"
+        icon="user-o"
+        iconSize={24}
+        iconPack={FontAwesome}
+        onInputChanged={inputChangedHandler}
+        errorText={formState.inputValidities["lastName"]}
+      />
+      <Input
+        id="email"
+        label="Email"
+        icon="mail"
+        autoCapitalize="none"
+        keyboardType="email-address"
+        iconSize={24}
+        iconPack={Feather}
+        onInputChanged={inputChangedHandler}
+        errorText={formState.inputValidities["email"]}
+      />
+      <Input
+        id="password"
+        label="Password"
+        icon="lock"
+        autoCapitalize="none"
+        secureTextEntry
+        iconSize={24}
+        iconPack={Feather}
+        onInputChanged={inputChangedHandler}
+        errorText={formState.inputValidities["password"]}
+      />
+      <SubmitButton
+        title="Sign Up"
+        style={styles.submitButton}
+        onPress={() => console.log("Button pressed")}
+        disabled={!formState.formIsValid}
+      />
+    </>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -73,6 +85,5 @@ const styles = StyleSheet.create({
     marginTop: 20,
   },
 });
-
 
 export default SignUpForm;
